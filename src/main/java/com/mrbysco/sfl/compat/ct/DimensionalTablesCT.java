4f345eee285159c2efeaa92c78a46e013ee722d8
Mrbysco <Mrbysco@users.noreply.github.com>
2023-04-13 17:27:18 +0200

    Re-enable crafttweaker support

--- src/main/java/com/mrbysco/sfl/compat/ct/DimensionalTablesCT.java
+++ src/main/java/com/mrbysco/sfl/compat/ct/DimensionalTablesCT.java
@@ -1,40 +1,43 @@
 package com.mrbysco.sfl.compat.ct;
 
+import com.blamejared.crafttweaker.api.CraftTweakerAPI;
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
 import com.mrbysco.sfl.init.MimicLootHandler;
 import net.minecraft.resources.ResourceLocation;
+import org.openzen.zencode.java.ZenCodeType;
 
 import java.util.List;
 
-//@ZenRegister
-//@Name("mods.cft.DimensionalTables")
+@ZenRegister
+@ZenCodeType.Name("mods.cft.DimensionalTables")
 public class DimensionalTablesCT {
-//    @Method
-    public static void addTable(String dimension, String lootTable) {
-//        CraftTweakerAPI.apply(new AddDimensionalTableAction(dimension, lootTable));
-    }
+	@ZenCodeType.Method
+	public static void addTable(String dimension, String lootTable) {
+		CraftTweakerAPI.apply(new AddDimensionalTableAction(dimension, lootTable));
+	}
 
-//    @Method
-    public static void removeTable(String dimension, String lootTable) {
-//        CraftTweakerAPI.apply(new AddDimensionalTableAction(dimension, lootTable));
-    }
+	@ZenCodeType.Method
+	public static void removeTable(String dimension, String lootTable) {
+		CraftTweakerAPI.apply(new AddDimensionalTableAction(dimension, lootTable));
+	}
 
-//    @Method
-    public static void addWaterTable(String lootTable) {
-//        CraftTweakerAPI.apply(new AddWaterTableAction(lootTable));
-    }
+	@ZenCodeType.Method
+	public static void addWaterTable(String lootTable) {
+		CraftTweakerAPI.apply(new AddWaterTableAction(lootTable));
+	}
 
-//    @Method
-    public static void removeWaterTable(String lootTable) {
-//        CraftTweakerAPI.apply(new RemoveWaterTableAction(lootTable));
-    }
+	@ZenCodeType.Method
+	public static void removeWaterTable(String lootTable) {
+		CraftTweakerAPI.apply(new RemoveWaterTableAction(lootTable));
+	}
 
-//    @Getter("tables")
-    public List<String> getTables(String dimension) {
-        return MimicLootHandler.getStringDimensionTables(new ResourceLocation(dimension));
-    }
+	@ZenCodeType.Getter("tables")
+	public List<String> getTables(String dimension) {
+		return MimicLootHandler.getStringDimensionTables(new ResourceLocation(dimension));
+	}
 
-//    @Getter("waterTables")
-    public List<String> getWaterTables() {
-        return MimicLootHandler.getStringWaterTables();
-    }
+	@ZenCodeType.Getter("waterTables")
+	public List<String> getWaterTables() {
+		return MimicLootHandler.getStringWaterTables();
+	}
 }
