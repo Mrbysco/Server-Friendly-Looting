239a1ea67e673af2a7b8da63102453b859a41688
Mrbysco <Mrbysco@users.noreply.github.com>
2023-08-05 15:39:32 +0200

    Re-enable crafttweaker support

--- src/main/java/com/mrbysco/sfl/compat/ct/AddWaterTableAction.java
+++ src/main/java/com/mrbysco/sfl/compat/ct/AddWaterTableAction.java
@@ -1,33 +1,38 @@
-//package com.mrbysco.sfl.compat.ct;
-//
-//import com.blamejared.crafttweaker.api.action.base.IUndoableAction;
-//import com.mrbysco.sfl.init.MimicLootHandler;
-//import net.minecraft.resources.ResourceLocation;
-//
-//public class AddWaterTableAction implements IUndoableAction {
-//	public final ResourceLocation lootTable;
-//
-//	public AddWaterTableAction(String table) {
-//		this.lootTable = new ResourceLocation(table);
-//	}
-//
-//	@Override
-//	public void apply() {
-//		MimicLootHandler.addWaterTable(lootTable);
-//	}
-//
-//	@Override
-//	public String describe() {
-//		return String.format("Loot table: " + lootTable.toString() + " has been added to the water mimic tables");
-//	}
-//
-//	@Override
-//	public void undo() {
-//		MimicLootHandler.removeWaterTable(lootTable);
-//	}
-//
-//	@Override
-//	public String describeUndo() {
-//		return String.format("Change to water mimic tables has been undone, " + lootTable.toString() + " has been removed from the list");
-//	}
-//}
+package com.mrbysco.sfl.compat.ct;
+
+import com.blamejared.crafttweaker.api.action.base.IUndoableAction;
+import com.mrbysco.sfl.init.MimicLootHandler;
+import net.minecraft.resources.ResourceLocation;
+
+public class AddWaterTableAction implements IUndoableAction {
+	public final ResourceLocation lootTable;
+
+	public AddWaterTableAction(String table) {
+		this.lootTable = new ResourceLocation(table);
+	}
+
+	@Override
+	public void apply() {
+		MimicLootHandler.addWaterTable(lootTable);
+	}
+
+	@Override
+	public String describe() {
+		return String.format("Loot table: " + lootTable.toString() + " has been added to the water mimic tables");
+	}
+
+	@Override
+	public void undo() {
+		MimicLootHandler.removeWaterTable(lootTable);
+	}
+
+	@Override
+	public String describeUndo() {
+		return String.format("Change to water mimic tables has been undone, " + lootTable.toString() + " has been removed from the list");
+	}
+
+	@Override
+	public String systemName() {
+		return "Server Friendly Looting";
+	}
+}
