fedd8fd8428f92711f2eb7333f1ce561c2ca6fe0
Mrbysco <Mrbysco@users.noreply.github.com>
2023-08-05 15:30:42 +0200

    Fix localization

--- src/main/java/com/mrbysco/sfl/datagen/SFLDatagen.java
+++ src/main/java/com/mrbysco/sfl/datagen/SFLDatagen.java
@@ -1,6 +1,7 @@
 package com.mrbysco.sfl.datagen;
 
 import com.mrbysco.sfl.ServerFriendlyLoot;
+import com.mrbysco.sfl.init.MimicRegistry;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.RegistrySetBuilder;
@@ -9,11 +10,14 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.registries.VanillaRegistries;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+import net.minecraftforge.common.data.LanguageProvider;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -25,9 +29,42 @@ public class SFLDatagen {
 		DataGenerator generator = event.getGenerator();
 		PackOutput packOutput = generator.getPackOutput();
 
-		if (event.includeServer()) {
-			generator.addProvider(event.includeServer(), new DatapackBuiltinEntriesProvider(
-					packOutput, CompletableFuture.supplyAsync(SFLDatagen::getProvider), Set.of(ServerFriendlyLoot.MOD_ID)));
+		generator.addProvider(event.includeClient(), new SFLLanguageProvider(packOutput));
+
+		generator.addProvider(event.includeServer(), new DatapackBuiltinEntriesProvider(
+				packOutput, CompletableFuture.supplyAsync(SFLDatagen::getProvider), Set.of(ServerFriendlyLoot.MOD_ID)));
+	}
+
+	public static class SFLLanguageProvider extends LanguageProvider {
+		public SFLLanguageProvider(PackOutput packOutput) {
+			super(packOutput, ServerFriendlyLoot.MOD_ID, "en_us");
+		}
+
+		@Override
+		protected void addTranslations() {
+			addItem(MimicRegistry.MIMIC_SPAWN_EGG, "Mimic Spawn Egg");
+			addItem(MimicRegistry.WATER_MIMIC_SPAWN_EGG, "Water Mimic Spawn Egg");
+			addItem(MimicRegistry.END_MIMIC_SPAWN_EGG, "End Mimic Spawn Egg");
+			addItem(MimicRegistry.NETHER_MIMIC_SPAWN_EGG, "Nether Mimic Spawn Egg");
+
+			addEntityType(MimicRegistry.MIMIC, "Mimic");
+			addEntityType(MimicRegistry.WATER_MIMIC, "Mimic");
+			addEntityType(MimicRegistry.END_MIMIC, "End Mimic");
+			addEntityType(MimicRegistry.NETHER_MIMIC, "Nether Mimic");
+
+			//TODO: Custom sounds
+//			addSubtitle(MimicSounds.MIMIC_AMBIENT, "Mimic noises");
+//			addSubtitle(MimicSounds.MIMIC_DEATH, "Mimic dies");
+//			addSubtitle(MimicSounds.MIMIC_HURT, "Mimic hurts");
+		}
+
+		public void addSubtitle(RegistryObject<SoundEvent> sound, String name) {
+			this.addSubtitle(sound.get(), name);
+		}
+
+		public void addSubtitle(SoundEvent sound, String name) {
+			String path = ServerFriendlyLoot.MOD_ID + ".subtitle." + sound.getLocation().getPath();
+			this.add(path, name);
 		}
 	}
 
